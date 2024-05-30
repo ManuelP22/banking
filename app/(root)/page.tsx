8ae5121ac6849032a0a5907c2bda97cb461238e8
Manuel Perez <51509197+ManuelP22@users.noreply.github.com>
2024-05-30 11:25:03 -0400

    implement file structure

---
+++ app/(root)/page.tsx
@@ -0,0 +1,22 @@
+import HeaderBox from '@/components/_components/HeaderBox'
+import React from 'react'
+
+const Home = () => {
+  const loggedIn = { firstName: 'Manuel' }
+  return (
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transaction efficiently."
+          />
+        </header>
+      </div>
+    </section>
+  )
+}
+
+export default Home
